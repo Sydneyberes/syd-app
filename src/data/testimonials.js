1cd21a0912445ec59efc791a0fe8bc65f500b5b5
Sydneyberes <you@personalemail.com>
2026-03-15 19:19:05 -0700

    Reorder testimonials: Brian, David, Cezar

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -2,26 +2,26 @@ const testimonials = [
   {
     id: 1,
     quote:
+      "Syd's become our tech org's superpower at Curri for exploring new ideas, simplifying complex ones, and making everything beautiful. I love how she keeps an open mind (always super curious) and then aces the problem.",
+    name: 'Brian Gonzalez',
+    title: 'CTO',
+    company: 'Curri',
+  },
+  {
+    id: 2,
+    quote:
       'Syd has an uncanny ability to get down to the core of an idea with simple yet functional designs.',
     name: 'David Lin',
     title: 'CEO',
     company: 'Duffl',
   },
   {
-    id: 2,
+    id: 3,
     quote:
       'Her research insights and relentless pursuit of a better customer experience have brought positive change.',
     name: 'Cezar Racheiru',
     title: 'VP Operations',
     company: 'Duffl',
-  },
-  {
-    id: 3,
-    quote:
-      "Syd's become our tech org's superpower at Curri for exploring new ideas, simplifying complex ones, and making everything beautiful. I love how she keeps an open mind (always super curious) and then aces the problem.",
-    name: 'Brian Gonzalez',
-    title: 'CTO',
-    company: 'Curri',
   },
 ]
 
